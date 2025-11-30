ea06cfa86e3bfeac1912c43dee94bad05d588365
Ketan-Barve <Ketanbarve89@gmail.com>
2025-11-30 15:31:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,18 @@ const text = "Ketan";
 let index = 0;
 
 function type() {
-    if (index < text.length) {
-        typedText.textContent += text.charAt(index);
-        index++;
-        setTimeout(type, 200);
-    }
+  if(index < text.length){
+    typedText.textContent += text.charAt(index);
+    index++;
+    setTimeout(type, 200);
+  }
 }
-
-typedText.textContent = ""; // Reset text
+typedText.textContent = "";
 type();
 
 // Dark Mode Toggle
 const toggleButton = document.getElementById('theme-toggle');
 toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    if(document.body.classList.contains('dark-mode')) {
-        toggleButton.textContent = '☀️';
-    } else {
-        toggleButton.textContent = '🌙';
-    }
+  document.body.classList.toggle('dark-mode');
+  toggleButton.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
 });
